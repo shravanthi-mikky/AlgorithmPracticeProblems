1b0391f3c303b13501c74b8ddd7bf3ca653cb548
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-13 16:31:39 +0530

    [shravanthi] -Added Program to detect if two words are Anagrams

---
+++ AlgorithmProblems/Anagram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    public class Anagram
+    {
+        public bool AnagramDetector(string First, string Second)
+        {
+            if (First.Length != Second.Length)
+            {
+                Console.WriteLine("Please enter two strings of same length!");
+                return false;
+            }
+            char[] FirstCharArray = First.ToLower().ToCharArray();
+            char[] SecondCharArray = Second.ToLower().ToCharArray();
+            Array.Sort(FirstCharArray);
+            Array.Sort(SecondCharArray);
+            for (int i = 0; i < FirstCharArray.Length; i++)
+            {
+                if (FirstCharArray[i] != SecondCharArray[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
